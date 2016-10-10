13601ec0b9baa2149f949ac239aa1c69a483b432
Andrew Mokotoff <amokotoff@gmail.com>
2016-10-10 10:28:37 -0400

    added main function and data variable, ready for branching

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import csv
 #library readMe: https://docs.python.org/2/library/csv.html
 
-with open('data/student-mat.csv', 'rb') as csvfile:
-	datareader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-	for row in datareader:
-		print ', '.join(row)
+def read_data(file_path):
+	with open('data/student-mat.csv', 'rb') as csvfile:
+		datareader = csv.reader(csvfile, delimiter=';', quotechar='|')
+		data = []
+		for row in datareader:
+			data.append(row)
+	return data
+
+if __name__ == "__main__":
+	data = read_data('data/student-mat.csv')	
+
