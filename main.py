b9ebefa85f69f2e790ce926e68cc209911617c30
Andrew Mokotoff <amokotoff@gmail.com>
2016-10-09 23:04:47 -0400

    added library URL in comments

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import csv
 #library readMe: https://docs.python.org/2/library/csv.html
 
 with open('data/student-mat.csv', 'rb') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-	for row in spamreader:
+	datareader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+	for row in datareader:
 		print ', '.join(row)
